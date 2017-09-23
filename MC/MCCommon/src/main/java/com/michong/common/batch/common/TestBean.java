19cf952a14306a7931c1d14a6e90b01cc9ba0414
liuxin_PC <liuxinxaut@163.com>
2017-09-23 20:37:50 +0800

    提交新建的项目目录，暂未开发任何功能

---
+++ MC/MCCommon/src/main/java/com/michong/common/batch/common/TestBean.java
@@ -0,0 +1,11 @@
+package com.michong.common.batch.common;
+
+/**
+ * Created by liuxin_PC on 2017/9/23.
+ */
+public class TestBean {
+    public static void main(String[] args){
+        System.out.println("Hello World !");
+
+    }
+}
