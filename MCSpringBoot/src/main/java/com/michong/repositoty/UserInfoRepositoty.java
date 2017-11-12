58e20d0a5c9149b648e54c7e108f2d2af9676aa6
liuxin_PC <liuxinxaut@163.com>
2017-11-12 21:04:34 +0800

    新增配置文件

--- MCSpringBoot/src/main/java/com/michong/repositoty/UserInfoRepositoty.java
+++ MCSpringBoot/src/main/java/com/michong/repositoty/UserInfoRepositoty.java
@@ -3,9 +3,13 @@ package com.michong.repositoty;
 
 import com.michong.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserInfoRepositoty extends JpaRepository<UserInfo, Long> {
 
+    @Query("SELECT t FROM UserInfo t WHERE userName =: name")
+    UserInfo findUserByName(@Param("name") String name);
 }
